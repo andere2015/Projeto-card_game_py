c186798337de96332016af1016ca69d9f468ff29
Anderson <115733205+andere2015@users.noreply.github.com>
2024-10-10 08:59:28 -0300

    Add files via upload

--- deck_builder.py
+++ deck_builder.py
@@ -32,7 +32,7 @@ def deck_builder(screen, player):
     selected_cards = {}
     available_cards = cartas_existentes
 
-    while sum(selected_cards.values()) < 20:
+    while sum(selected_cards.values()) < 5:
         screen.fill(WHITE)
         draw_text(f'{player.name}, escolha suas cartas ({sum(selected_cards.values())}/20)', font, BLACK, screen, 364, 50)
         draw_card_options(screen, available_cards, selected_cards)
